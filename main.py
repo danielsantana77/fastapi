de11b11e2cd1f57e317c917fe9a48154deb8b83c
Daniel Oliveira <30814811+danielsantana77@users.noreply.github.com>
2024-02-15 16:06:41 -0300

    feat : busca de contas a pagar a partir de um fornecedor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,Request
 import uvicorn
-from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router, fornecedor_cliente_router
+from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router, fornecedor_cliente_router, \
+    fornecedor_cliente_vs_contas_router
 from shared.database import engine,Base
 from shared.exceptions import NotFound
 from shared.exceptions_handler import not_found_exception_handler
@@ -19,6 +20,7 @@ def hello_world():
 
 app.include_router(contas_a_pagar_e_receber_router.router)
 app.include_router(fornecedor_cliente_router.router)
+app.include_router(fornecedor_cliente_vs_contas_router.router)
 app.add_exception_handler(NotFound,not_found_exception_handler)
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
